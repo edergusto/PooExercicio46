81d38e52649ec115541d295e3109f04a070848f6
Eder UniSENAI <eder.a.silva@mt.estudante.senai.br>
2026-05-04 20:47:08 -0400

    C1: A saída do código demonstra um endereço de memória ilegível.

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import br.com.Produto;
+
+public class Main {
+    public static void main(String[] args) {
+        Produto a1 = new Produto("Boné", 10.0);
+        Produto b1 = new Produto("Camiseta", 29.90);
+
+        System.out.println(a1);
+        System.out.println(b1);
+    }
+}
---
+++ src/br/com/Produto.java
@@ -0,0 +1,9 @@
+package br.com;
+
+public class Produto {
+    String nome;
+    double preco;
+
+    public Produto(String produto, double valor) {
+    }
+}
